cde125a8ea5ac6247be59e6e53db319293e57e55
yzzzwan <kyw011012@gmail.com>
2023-04-08 02:51:27 +0900

    haksik and bus distribute

--- haksik/crawl.py
+++ haksik/crawl.py
@@ -3,20 +3,20 @@ import time
 from selenium.webdriver.common.by import By
 
 # local
-driver = webdriver.ChromeOptions()
-driver.add_experimental_option("excludeSwitches", ["enable-logging"])
-driver = webdriver.Chrome('C:\\Users\\kyw01\\Downloads\\chromedriver_win32\\chromedriver')
-driver.get('https://www.anyang.ac.kr/main/activities/school-cafeteria.do')
-#time.sleep(1)
+# driver = webdriver.ChromeOptions()
+# driver.add_experimental_option("excludeSwitches", ["enable-logging"])
+# driver = webdriver.Chrome('C:\\Users\\kyw01\\Downloads\\chromedriver_win32\\chromedriver')
+# driver.get('https://www.anyang.ac.kr/main/activities/school-cafeteria.do')
+# #time.sleep(1)
 
 #server
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument('--headless')
-# chrome_options.add_argument('--no-sandbox')
-# chrome_options.add_argument('--disable-dev-shm-usage')
-# driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver",chrome_options=chrome_options)
-# driver.get('https://www.anyang.ac.kr/main/activities/school-cafeteria.do')
-# # time.sleep(1)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver",chrome_options=chrome_options)
+driver.get('https://www.anyang.ac.kr/main/activities/school-cafeteria.do')
+# time.sleep(1)
 
 
 
