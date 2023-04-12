0463bc00e79d12a4096e830544f51df6caee690a
yzzzwan <kyw011012@gmail.com>
2023-04-13 02:11:33 +0900

    study room test

--- apps/views.py
+++ apps/views.py
@@ -96,165 +96,164 @@ def phoneDir(request):
         }
     })
 
-from . import test
-
-# self 학습실 시간표 보여주기
-@csrf_exempt
-def studyRoom(request):
-    answer = ((request.body).decode('utf-8'))
-    json_str = str(json.loads(answer))
-    # return_str = json_str['userRequest']['utterance'] # 사용자의 발화 텍스트
-    # phoneBook = pdc.find_dept(return_str)
-    room="Self 학습실1(Career design)"
-    times = test.test
-
-    if len(times) <= 5:
-        return JsonResponse({
-            'version': "2.0",
-             "template": {
-                "outputs": [
-                  {
-                    "listCard": {
-                      "header": {
-                        "title": room+"의 이용 가능 시간"
-                      },
-                        "items": [
-                            {
-                                "title": test.test[0],
-                            },
-
-                            {
-                                "title": test.test[1],
-                            },
-
-                            {
-                                "title": test.test[2],
-                            },
-
-                            {
-                                "title": test.test[3],
-                            },
-
-                            {
-                                "title": test.test[4],
-                            },
-                        ],
-                    }
-                  }]
-             }
-        })
-
-    elif len(times) <= 10:
-        return JsonResponse({
-            'version': "2.0",
-             "template": {
-                "outputs": [
-                  {
-                    "listCard": {
-                      "header": {
-                        "title": room+"의 이용 가능 시간2"
-                      },
-                        "items": [
-                            {
-                                "title": test.test[0],
-                            },
-
-                            {
-                                "title": test.test[1],
-                            },
-
-                            {
-                                "title": test.test[2],
-                            },
-
-                            {
-                                "title": test.test[3],
-                            },
-
-                            {
-                                "title": test.test[4],
-                            },
-
-                            {
-                                "title": test.test[5],
-                            },
-
-                            {
-                                "title": test.test[6],
-                            },
-
-                            {
-                                "title": test.test[7],
-                            },
-
-                            {
-                                "title": test.test[8],
-                            },
-
-                            {
-                                "title": test.test[9],
-                            },
-                        ],
-
-                    }
-                  }]
-             }
-        })
-
-    else:
-        return JsonResponse({
-            'version': "2.0",
-            "template": {
-                "outputs": [
-                    {
-                        "listCard": {
-                            "header": {
-                                "title": room + "의 이용 가능 시간2"
-                            },
-                            "items": [
-                                {
-                                    "title": test.test[0],
-                                },
-                                {
-                                    "title": test.test[1],
-                                },
-                                {
-                                    "title": test.test[2],
-                                },
-                                {
-                                    "title": test.test[3],
-                                },
-                                {
-                                    "title": test.test[4],
-                                },
-                                {
-                                    "title": test.test[5],
-                                },
-                                {
-                                    "title": test.test[6],
-                                },
-                                {
-                                    "title": test.test[7],
-                                },
-                                {
-                                    "title": test.test[8],
-                                },
-                                {
-                                    "title": test.test[9],
-                                },
-                                {
-                                    "title": test.test[10],
-                                },
-                                {
-                                    "title": test.test[11],
-                                },
-                                {
-                                    "title": test.test[12],
-                                },
-                            ],
-                        }
-                    }]
-            }
-        })
+# from . import test
+#
+# # self 학습실 시간표 보여주기
+# @csrf_exempt
+# def studyRoom(request):
+#     answer = ((request.body).decode('utf-8'))
+#     json_str = str(json.loads(answer))
+#     # return_str = json_str['userRequest']['utterance'] # 사용자의 발화 텍스트
+#     # phoneBook = pdc.find_dept(return_str)
+#     room="Self 학습실1(Career design)"
+#     times = test.test
+#     if len(times) <= 5:
+#         return JsonResponse({
+#             'version': "2.0",
+#             "template": {
+#                 "outputs": [
+#                   {
+#                       "listCard": {
+#                       "header": {
+#                         "title": room+"의 이용 가능 시간"
+#                       },
+#                         "items": [
+#                             {
+#                                 "title": test.test[0],
+#                             },
+#
+#                             {
+#                                 "title": test.test[1],
+#                             },
+#
+#                             {
+#                                 "title": test.test[2],
+#                             },
+#
+#                             {
+#                                 "title": test.test[3],
+#                             },
+#
+#                             {
+#                                 "title": test.test[4],
+#                             },
+#                         ],
+#                     }
+#                   }]
+#              }
+#         })
+#
+#     elif len(times) <= 10:
+#         return JsonResponse({
+#             'version': "2.0",
+#              "template": {
+#                 "outputs": [
+#                   {
+#                     "listCard": {
+#                       "header": {
+#                         "title": room+"의 이용 가능 시간2"
+#                       },
+#                         "items": [
+#                             {
+#                                 "title": test.test[0],
+#                             },
+#
+#                             {
+#                                 "title": test.test[1],
+#                             },
+#
+#                             {
+#                                 "title": test.test[2],
+#                             },
+#
+#                             {
+#                                 "title": test.test[3],
+#                             },
+#
+#                             {
+#                                 "title": test.test[4],
+#                             },
+#
+#                             {
+#                                 "title": test.test[5],
+#                             },
+#
+#                             {
+#                                 "title": test.test[6],
+#                             },
+#
+#                             {
+#                                 "title": test.test[7],
+#                             },
+#
+#                             {
+#                                 "title": test.test[8],
+#                             },
+#
+#                             {
+#                                 "title": test.test[9],
+#                             },
+#                         ],
+#
+#                     }
+#                   }]
+#              }
+#         })
+#
+#     else:
+#         return JsonResponse({
+#             'version': "2.0",
+#             "template": {
+#                 "outputs": [
+#                     {
+#                         "listCard": {
+#                             "header": {
+#                                 "title": room + "의 이용 가능 시간2"
+#                             },
+#                             "items": [
+#                                 {
+#                                     "title": test.test[0],
+#                                 },
+#                                 {
+#                                     "title": test.test[1],
+#                                 },
+#                                 {
+#                                     "title": test.test[2],
+#                                 },
+#                                 {
+#                                     "title": test.test[3],
+#                                 },
+#                                 {
+#                                     "title": test.test[4],
+#                                 },
+#                                 {
+#                                     "title": test.test[5],
+#                                 },
+#                                 {
+#                                     "title": test.test[6],
+#                                 },
+#                                 {
+#                                     "title": test.test[7],
+#                                 },
+#                                 {
+#                                     "title": test.test[8],
+#                                 },
+#                                 {
+#                                     "title": test.test[9],
+#                                 },
+#                                 {
+#                                     "title": test.test[10],
+#                                 },
+#                                 {
+#                                     "title": test.test[11],
+#                                 },
+#                                 {
+#                                     "title": test.test[12],
+#                                 },
+#                             ],
+#                         }
+#                     }]
+#             }
+#         })
 
 
