f985d864ebee55092a135704b49be74c478bc97a
yzzzwan <kyw011012@gmail.com>
2023-04-19 02:41:56 +0900

    portal login test

--- apps/a.py
+++ apps/a.py
@@ -1,10 +1,10 @@
-import portal_login_user
-import studyroom_Timetable
-
-print("start a")
-a=portal_login_user.portal("2020E7011","rladyddhks1!")
-print("end a")
-print("start b")
-b=studyroom_Timetable.show_studyroom_timetable("1")
-print("end b")
-
+# import portal_login_user
+# import studyroom_Timetable
+#
+# print("start a")
+# a=portal_login_user.portal("2020E7011","rladyddhks1!")
+# print("end a")
+# print("start b")
+# b=studyroom_Timetable.show_studyroom_timetable("1")
+# print("end b")
+#
