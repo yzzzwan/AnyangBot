724656939463bf042ec4830b9a2557bc31e3a581
yzzzwan <kyw011012@gmail.com>
2023-04-22 03:39:47 +0900

    stduyroom timetable test

--- apps/views.py
+++ apps/views.py
@@ -51,12 +51,14 @@ def week_haksik(request):
     return_json_str = json.loads(answer)
     return_str = return_json_str['userRequest']['utterance'] # 사용자의 발화 텍스트
 
+    week_haksik_menu = haksik_crawl.print_week_haksik()
+
     return JsonResponse({
         'version': "2.0",
         'template': {
             'outputs': [{
                 'simpleText': {
-                    'text': haksik_crawl.week_menu
+                    'text': week_haksik_menu
                 }
             }],
         }
@@ -69,12 +71,14 @@ def today_haksik(request):
     return_json_str = json.loads(answer)
     return_str = return_json_str['userRequest']['utterance'] # 사용자의 발화 텍스트
 
+    today_haksik_menu = haksik_crawl.print_today_haksik()
+
     return JsonResponse({
         'version': "2.0",
         'template': {
             'outputs': [{
                 'simpleText': {
-                    'text': haksik_crawl.today_menu
+                    'text': today_haksik_menu
                 }
             }]
         }
