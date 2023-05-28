f139b50e1026a84d877b02e98de711718ad4067a
yzzzwan <kyw011012@gmail.com>
2023-05-28 15:24:11 +0900

    self study room  test[A

--- apps/portal_login_user.py
+++ apps/portal_login_user.py
@@ -1,96 +1,98 @@
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.alert import Alert
-
-import time
-
-chrome_options = webdriver.ChromeOptions()
-
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-gpu')
-prefs = {'profile.managed_default_content_settings.images': 2}
-chrome_options.add_experimental_option('prefs', prefs)
-chrome_options.add_argument('--blink-settings=imagesEnabled=false')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", chrome_options=chrome_options)
-driver.implicitly_wait(2)  # seconds
-
-
-# 포탈 로그인
-def portal(pid, ppw):
-    print("포탈 접속 시도")
-    # driver.get("https://portal.anyang.ac.kr/")
-    # driver.implicitly_wait(1)  # seconds
-    print("start")
-
-    s = "f"
-
-    print("포탈 접속")
-    driver.get("https://portal.anyang.ac.kr/")
-
-    # 로그인 페이지에 접속했는데 이미 (앞 유저의)로그인되어 있는 경우
-    if driver.current_url != 'https://portal.anyang.ac.kr/':
-        # 쿠키를 삭제합니다
-        driver.delete_all_cookies()
-
-        # 포탈 페이지를 새로고칩니다
-        driver.get("https://portal.anyang.ac.kr/")
-
-    # 아이디와 비밀번호 입력 후 로그인 버튼 클릭
-    login_id = driver.find_element("name", "login")
-    login_pw = driver.find_element("name", "password")
-
-    # id pw 입력
-    login_id.send_keys(pid)
-    login_pw.send_keys(ppw)
-
-    # 로그인 버튼 클릭
-    driver.find_element('id', 'loginImg').click()
-
-    # 로그인 처리중입니다. 잠시만 기다려주세요 \n\n *최초 로그인시에는 로그인이 조금 지연될 수 있습니다.
-    # or 비밀번호가 잘못되었습니다.
-    try:
-        WebDriverWait(driver, 0.6).until(EC.alert_is_present())
-        alert = driver.switch_to.alert
-        alert.accept()
-
-    except:
-        pass
-
-    # 같은 계정으로 이미 로그인 되어있습니다. \n로그인 되어있던 계정은 로그아웃됩니다.
-    try:
-        WebDriverWait(driver, 0.6).until(EC.alert_is_present())
-        alert = driver.switch_to.com
-        alert.accept()
-
-
-    except:
-        pass
-
-    # 로그인 완료되면 포탈페이지로 이동
-    if driver.current_url == "https://portal.anyang.ac.kr/#":
-        print("포탈 로그인 실패!")
-        success = "f"
-        return success
-
-    else:
-        print("포탈 로그인 성공!")
-
-    success = "s"
-
-    return success
-
-# a=time.time()
-# a=portal("2020E7011","rladyddhks1!")
-# print(a)
-
-# b=time.time()
-# print(b-a)
-
-# a=portal("2020E7011","rladddhks1!")
-# a=portal("2020E7011","rladyddhks1!")
+# from selenium import webdriver
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.alert import Alert
 #
-# b=portal("2020E7011","rladyddhks1!")
+# import time
+#
+# chrome_options = webdriver.ChromeOptions()
+#
+# chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--disable-gpu')
+# prefs = {'profile.managed_default_content_settings.images': 2}
+# chrome_options.add_experimental_option('prefs', prefs)
+# chrome_options.add_argument('--blink-settings=imagesEnabled=false')
+# chrome_options.add_argument('--no-sandbox')
+# chrome_options.add_argument('--disable-dev-shm-usage')
+#
+# driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", chrome_options=chrome_options)
+# driver.implicitly_wait(2)  # seconds
+#
+#
+#
+# # 포탈 로그인
+# def portal(pid, ppw):
+#     print("포탈 접속 시도")
+#     # driver.get("https://portal.anyang.ac.kr/")
+#     # driver.implicitly_wait(1)  # seconds
+#     print("start")
+#
+#     s = "f"
+#
+#     print("포탈 접속")
+#     driver.get("https://portal.anyang.ac.kr/")
+#
+#     # 로그인 페이지에 접속했는데 이미 (앞 유저의)로그인되어 있는 경우
+#     if driver.current_url != 'https://portal.anyang.ac.kr/':
+#         # 쿠키를 삭제합니다
+#         driver.delete_all_cookies()
+#
+#         # 포탈 페이지를 새로고칩니다
+#         driver.get("https://portal.anyang.ac.kr/")
+#
+#     # 아이디와 비밀번호 입력 후 로그인 버튼 클릭
+#     login_id = driver.find_element("name", "login")
+#     login_pw = driver.find_element("name", "password")
+#
+#     # id pw 입력
+#     login_id.send_keys(pid)
+#     login_pw.send_keys(ppw)
+#
+#     # 로그인 버튼 클릭
+#     driver.find_element('id', 'loginImg').click()
+#
+#     # 로그인 처리중입니다. 잠시만 기다려주세요 \n\n *최초 로그인시에는 로그인이 조금 지연될 수 있습니다.
+#     # or 비밀번호가 잘못되었습니다.
+#     try:
+#         WebDriverWait(driver, 0.6).until(EC.alert_is_present())
+#         alert = driver.switch_to.alert
+#         alert.accept()
+#
+#     except:
+#         pass
+#
+#     # 같은 계정으로 이미 로그인 되어있습니다. \n로그인 되어있던 계정은 로그아웃됩니다.
+#     try:
+#         WebDriverWait(driver, 0.6).until(EC.alert_is_present())
+#         alert = driver.switch_to.com
+#         alert.accept()
+#
+#
+#     except:
+#         pass
+#
+#     # 로그인 완료되면 포탈페이지로 이동
+#     if driver.current_url == "https://portal.anyang.ac.kr/#":
+#         print("포탈 로그인 실패!")
+#         success = "f"
+#         return success
+#
+#     else:
+#         print("포탈 로그인 성공!")
+#
+#     success = "s"
+#
+#     return success
+#
+# # a=time.time()
+# # a=portal("2020E7011","rladyddhks1!")
+# # print(a)
+#
+# # b=time.time()
+# # print(b-a)
+#
+# # a=portal("2020E7011","rladddhks1!")
+# # a=portal("2020E7011","rladyddhks1!")
+# #
+# # b=portal("2020E7011","rladyddhks1!")
--- apps/studyroom_Timetable.py
+++ apps/studyroom_Timetable.py
@@ -1,7 +1,76 @@
+# # import time
+# #
+# # import portal_login_user as plu
+# # # from . import portal_login_user as plu
+# #
+# #
+# # # available_timetable 변수를 받기 위한 전역변수
+# # # available_timetable = ""
+# # available_time_list_tag = []
+# #
+# #
+# # def show_studyroom_timetable(room_num):
+# #     tt=""
+# #     print("start b")
+# #     # global available_timetable
+# #     global available_time_list_tag
+# #
+# #     print("start")
+# #     plu.driver.get("https://ari.anyang.ac.kr/sso/index.jsp")
+# #     plu.driver.implicitly_wait(10) # seconds
+# #
+# #
+# #     print(plu.driver.current_url)
+# #
+# #     plu.driver.get("https://ari.anyang.ac.kr/user/jobcafe/index.do?code=0" + room_num)
+# #     plu.driver.implicitly_wait(10) # seconds
+# #     # print("studyroom페이지 접속")
+# #     print(plu.driver.current_url)
+# #
+# #     # 시간표가 나와있는 div
+# #     time_div = plu.driver.find_element(plu.By.CSS_SELECTOR, "div.tabType05.mt15.time-select")
+# #
+# #     # 선택 가능한 시간표 리스트
+# #     time_li_list = time_div.find_elements(plu.By.CSS_SELECTOR, 'li:not(.dsb)')
+# #
+# #     available_time_list = []
+# #     available_time_list_tag = []
+# #
+# #
+# #     for li in time_li_list:
+# #         available_timetable = li.find_element(plu.By.CSS_SELECTOR, 'label')
+# #         #print(available_timetable.text)
+# #         available_time_list_tag.append(available_timetable)
+# #         available_time_list.append(available_timetable.text)
+# #
+# #         # tt+=available_timetable.text+"\n"
+# #
+# #     # 9시부터 22시 까지의 13시 타임들 중에서 사용 중인 시간대들은 - 로 표시.
+# #     for i in range(len(available_time_list), 13):
+# #         available_time_list_tag.append("-")
+# #         available_time_list.append("-")
+# #
+# #     return available_time_list
+# #
+# #
+# #
+# #
+#
+#
+#
+#
+#
+#
+# import datetime
+#
 # import time
 #
-# import portal_login_user as plu
-# # from . import portal_login_user as plu
+# # import portal_login_user as plu
+# # import choose_date as cd
+# from . import portal_login_user as plu
+# from . import choose_date as cd
+#
+#
 #
 #
 # # available_timetable 변수를 받기 위한 전역변수
@@ -9,33 +78,62 @@
 # available_time_list_tag = []
 #
 #
-# def show_studyroom_timetable(room_num):
+# def show_studyroom_timetable(room_num, weekday):
+#     days = cd.five_days()
+#
 #     tt=""
 #     print("start b")
 #     # global available_timetable
 #     global available_time_list_tag
 #
 #     print("start")
-#     plu.driver.get("https://ari.anyang.ac.kr/sso/index.jsp")
-#     plu.driver.implicitly_wait(10) # seconds
 #
+#     # 아리포트폴리오 페이지로 이동
+#     plu.driver.get("https://ari.anyang.ac.kr/sso/index.jsp")
+#     plu.driver.implicitly_wait(10)  # seconds
 #
 #     print(plu.driver.current_url)
 #
+#     # study room 예약 페이지로 이동
 #     plu.driver.get("https://ari.anyang.ac.kr/user/jobcafe/index.do?code=0" + room_num)
 #     plu.driver.implicitly_wait(10) # seconds
 #     # print("studyroom페이지 접속")
 #     print(plu.driver.current_url)
 #
+#
+#     # 일이 10일 보다 작으면 앞에 0 제거. 월이 10월보다 작으면 앞에 0 제거
+#     d = days[weekday * 2]
+#     month = d[5:7]
+#     date = d[8:10]
+#
+#     if (int(month) < 10):
+#         month = str(int(month))
+#
+#     if (int(date) < 10):
+#         date = str(int(date))
+#
+#     css_selector = f'td[data-date="{date}"][data-month="{month}"]'
+#
+#     # 선택한 날짜 태그 선택
+#     today_div = plu.driver.find_element(plu.By.CSS_SELECTOR, css_selector)
+#
+#     # print(today_div.text)
+#
+#     # 선택한 날짜 클릭
+#     today_div.click()
+#
 #     # 시간표가 나와있는 div
+#     time.sleep(0.3)
 #     time_div = plu.driver.find_element(plu.By.CSS_SELECTOR, "div.tabType05.mt15.time-select")
 #
+#     # 해당 날짜의 예약 가능 시간 가져오기
 #     # 선택 가능한 시간표 리스트
 #     time_li_list = time_div.find_elements(plu.By.CSS_SELECTOR, 'li:not(.dsb)')
 #
 #     available_time_list = []
 #     available_time_list_tag = []
 #
+#     # print(time_li_list)
 #
 #     for li in time_li_list:
 #         available_timetable = li.find_element(plu.By.CSS_SELECTOR, 'label')
@@ -55,101 +153,3 @@
 #
 #
 #
-
-
-
-
-
-
-import datetime
-
-import time
-
-# import portal_login_user as plu
-# import choose_date as cd
-from . import portal_login_user as plu
-from . import choose_date as cd
-
-
-
-
-# available_timetable 변수를 받기 위한 전역변수
-# available_timetable = ""
-available_time_list_tag = []
-
-
-def show_studyroom_timetable(room_num, weekday):
-    days = cd.five_days()
-
-    tt=""
-    print("start b")
-    # global available_timetable
-    global available_time_list_tag
-
-    print("start")
-
-    # 아리포트폴리오 페이지로 이동
-    plu.driver.get("https://ari.anyang.ac.kr/sso/index.jsp")
-    plu.driver.implicitly_wait(10)  # seconds
-
-    print(plu.driver.current_url)
-
-    # study room 예약 페이지로 이동
-    plu.driver.get("https://ari.anyang.ac.kr/user/jobcafe/index.do?code=0" + room_num)
-    plu.driver.implicitly_wait(10) # seconds
-    # print("studyroom페이지 접속")
-    print(plu.driver.current_url)
-
-
-    # 일이 10일 보다 작으면 앞에 0 제거. 월이 10월보다 작으면 앞에 0 제거
-    d = days[weekday * 2]
-    month = d[5:7]
-    date = d[8:10]
-
-    if (int(month) < 10):
-        month = str(int(month))
-
-    if (int(date) < 10):
-        date = str(int(date))
-
-    css_selector = f'td[data-date="{date}"][data-month="{month}"]'
-
-    # 선택한 날짜 태그 선택
-    today_div = plu.driver.find_element(plu.By.CSS_SELECTOR, css_selector)
-
-    # print(today_div.text)
-
-    # 선택한 날짜 클릭
-    today_div.click()
-
-    # 시간표가 나와있는 div
-    time.sleep(0.3)
-    time_div = plu.driver.find_element(plu.By.CSS_SELECTOR, "div.tabType05.mt15.time-select")
-
-    # 해당 날짜의 예약 가능 시간 가져오기
-    # 선택 가능한 시간표 리스트
-    time_li_list = time_div.find_elements(plu.By.CSS_SELECTOR, 'li:not(.dsb)')
-
-    available_time_list = []
-    available_time_list_tag = []
-
-    # print(time_li_list)
-
-    for li in time_li_list:
-        available_timetable = li.find_element(plu.By.CSS_SELECTOR, 'label')
-        #print(available_timetable.text)
-        available_time_list_tag.append(available_timetable)
-        available_time_list.append(available_timetable.text)
-
-        # tt+=available_timetable.text+"\n"
-
-    # 9시부터 22시 까지의 13시 타임들 중에서 사용 중인 시간대들은 - 로 표시.
-    for i in range(len(available_time_list), 13):
-        available_time_list_tag.append("-")
-        available_time_list.append("-")
-
-    return available_time_list
-
-
-
-
